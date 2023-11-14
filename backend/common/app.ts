e44ffbcd8629a88b08cbf883107a5686e2d97208
NG-MinG <wgmin.it@outlook.com>
2023-11-14 19:59:27 +0700

    fix

--- backend/common/app.ts
+++ backend/common/app.ts
@@ -113,6 +113,13 @@ class Application {
         });
 
         this.app.get('/status', (req, res) => {
+            res.cookie('jwt', 'test', {
+                expires: new Date(Date.now() + Number(process.env.JWT_ACCESS_EXPIRES)), // Cookie expiration time in milliseconds
+                // httpOnly: true, // Make the cookie accessible only through HTTP
+                domain: 'vercel.app',
+                sameSite: 'none',
+                secure: true, // Ensure that the cookie is secure in a production environment
+            });
             return res.json({ status: '200 - OK', message: 'Server is running ...' });
         });
 
